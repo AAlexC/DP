b33132dfa2e368dc44258d106801302747921688
Ulises Alejandro Castro Bustos <ulisescastro2015@gmail.com>
2017-10-31 16:43:59 -0600

    Update XboxLivePoints.cpp

--- XboxLivePoints.cpp
+++ XboxLivePoints.cpp
@@ -5,20 +5,20 @@ int Cajas[MAXN];
 int Suma=0, Aux, N, guarda = 0, final, i;
 int main(){	 
 	cin >> N;	
-	for(int i=1;i<=N;i++){ 
+	for(int i = 1;i <= N; i++){ 
 	 	if(Suma < 0) {
 	       	Suma = 0;            
 		guarda++;
 		}
 	cin >> Aux;
 	if(Aux < 0) guarda++;
-	Suma+=Aux;	
-	Cajas[guarda]=Suma;			  
+	 	Suma += Aux;	
+		Cajas[guarda]=Suma;			  
 	}
 	final =- 10000;
-   for(i = 0;i <= guarda; i++){  	
-   	 if(Cajas[i]>final) final = Cajas[i];   	 			
-   }	
+	for(i = 0;i <= guarda; i++){  	
+   		if(Cajas[i] > final) final = Cajas[i];   	 			
+  	}	
 	cout << final<<"\n";	
 	return 0;
 }
